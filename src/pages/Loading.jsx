9f78f9ee3ec5f6a480a36ced2ac0086b8b438713
rcosta349 <rafael.costa2109@gmail.com>
2023-11-10 15:03:33 +0000

    loading page update

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 
 function Loading() {
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex',alignItems:'center',justifyContent:'center'}}>
           <CircularProgress />
         </Box>
       );
