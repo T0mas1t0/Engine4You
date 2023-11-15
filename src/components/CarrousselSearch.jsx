dec008a479e27e8ac29e52cf14d8c14c56f16b08
BrunoDavidES <bm.david@campus.fct.unl.pt>
2023-11-15 11:14:58 +0000

    branch with carousel

--- src/components/CarrousselSearch.jsx
+++ src/components/CarrousselSearch.jsx
@@ -1,5 +1,69 @@
-function CarrousselSearch() {
-    return ("Carroussel");
-}
+import React from "react";
+import {Swiper, SwiperSlide} from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-export default CarrousselSearch;
+import {EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
+
+import slide_img from "../assets/model.jpg";
+
+export default function CarouselSearch() {
+    return (
+        <div className="container" >
+        <Swiper
+        direction='horizontal'
+        effect = {'coverflow'}
+        grabCursor = {true}
+        centeredSlides = {true}
+        loop = {true}
+        slidesPerView = {'auto'}
+        coverflowEffect = {
+            {
+                rotate: 0,
+                stretch:0,
+                depth:100,
+                modifier:2.5  
+            }
+        }
+        pagination={{el:'.swiper-pagination', clickable:true}}
+        navigation={
+            {
+                
+                nextEl:'.swiper-button-next',
+                prevEl:'.swiper-button-prev',
+                clickable: true,
+            
+            }
+        }
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+        >
+            <SwiperSlide className='images'>
+                <img src={slide_img} alt="slide_image"/>
+            </SwiperSlide>
+            <SwiperSlide className='images'>
+                <img src={slide_img} alt="slide_image"/>
+            </SwiperSlide>
+            <SwiperSlide className='images'>
+                <img src={slide_img} alt="slide_image"/>
+            </SwiperSlide>
+            <SwiperSlide className='images'>
+                <img src={slide_img} alt="slide_image"/>
+            </SwiperSlide>
+            <SwiperSlide className='images'>
+                <img src={slide_img} alt="slide_image"/>
+            </SwiperSlide>
+            <div className="slider_controller">
+                <div className="swiper_button_prev slide slide_arrow">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                </div>
+                <div className="swiper_button_next slide slide_arrow">
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                </div> 
+            </div>
+        </Swiper>
+    </div>
+);
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import Search from '../components/Search/Search';
+import CarouselSearch from '../components/CarrousselSearch';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,14 +20,14 @@ function Home() {
     return (
         <Box sx={{ flexGrow: 1, marginTop:"100px" }}>
           <Grid container spacing={2}>
-            <Grid xs={12} md={5}>
+            <Grid xs={12} md={4}>
 
               <Search/>
 
             </Grid>
-            <Grid xs={12} md={7}>
+            <Grid xs={12} md={8}>
 
-              <Item>xs=12 md=7</Item>
+             <CarouselSearch/>
               
             </Grid>
             
